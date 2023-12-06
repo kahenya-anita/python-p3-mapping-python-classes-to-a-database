ebeeecfc7d6b6efcf7776467d17898c1f258f3e7
anita-that-chiq <62019551+kahenya-anita@users.noreply.github.com>
2023-12-06 10:13:14 +0300

    ORM

--- lib/testing/class_test.py
+++ lib/testing/class_test.py
@@ -1,4 +1,4 @@
-from lib.config import CURSOR
+from lib import CURSOR
 from lib.song import Song
 
 class TestClass:
@@ -16,11 +16,6 @@ class TestClass:
 
     def test_saves_song_to_table(self):
         '''has instancemethod "save()" that saves a song to music.db.'''
-
-        
-        CURSOR.execute('''DROP TABLE IF EXISTS songs''')
-        Song.create_table()
-
         song = Song("Hold On", "Born to Sing")
         song.save()
         db_song = CURSOR.execute(
@@ -29,16 +24,11 @@ class TestClass:
         ).fetchone()
         assert(db_song[1] == song.name and db_song[2] == song.album)
 
-    def test_creates_and_returns_song(self):
+    def creates_and_returns_song(self):
         '''has classmethod "create()" that creates a Song instance, saves it, and returns it.'''
-
-        CURSOR.execute('''DROP TABLE IF EXISTS songs''')
-        Song.create_table()
-
         song = Song.create("Hold On", "Born to Sing")
         db_song = CURSOR.execute(
             'SELECT * FROM songs WHERE name=? AND album=?',
             ('Hold On', 'Born to Sing')
         ).fetchone()
-
-        assert(db_song[0] == song.id and db_song[1] == song.name and db_song[2] == song.album)
+        assert(db_song[0] == song[0] and db_song[1] == song[1] and db_song[2] == song[2])
